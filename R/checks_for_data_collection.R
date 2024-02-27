a98fae2ad08dca736655d4a1a0c30e999d132591
Anthony Twesigye <twesigye10@gmail.com>
2024-02-27 12:23:54 +0300

    updated datasets

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -8,26 +8,26 @@ source("support_files/credentials.R")
 
 # read data ---------------------------------------------------------------
 
-loc_data <- "inputs/UGA2305_land_and_energy_data.xlsx"
-
-df_tool_data <- readxl::read_excel(loc_data)
+loc_data_refugee <- "inputs/UGA2305_land_and_energy_data.xlsx"
+df_tool_data_refugee <- readxl::read_excel(loc_data_refugee)
 
 # tool
-loc_tool <- "inputs/land_and_energy_tool.xlsx"
-df_survey <- readxl::read_excel(loc_tool, sheet = "survey") 
-df_choices <- readxl::read_excel(loc_tool, sheet = "choices")
+loc_tool_refugee <- "inputs/land_and_energy_tool.xlsx"
+df_survey_refugee <- readxl::read_excel(loc_tool_refugee, sheet = "survey") 
+df_choices_refugee <- readxl::read_excel(loc_tool_refugee, sheet = "choices")
 
 
 # download audit files
-download_audit_files(df = df_tool_data, 
+download_audit_files(df = df_tool_data_refugee, 
                      uuid_column = "_uuid", 
-                     audit_dir = "inputs/audit_files", 
+                     audit_dir = "inputs/audit_files_refugee", 
                      usr = user_acc, 
                      pass = user_pss)
 # zip audit files folder
-if (dir.exists("inputs/audit_files")) {
-    zip::zip(zipfile = "inputs/audit_files.zip", 
-             # files = "inputs/audit_files/",
-             files = list.dirs(path = "inputs/audit_files/", full.names = TRUE, recursive = FALSE),
+if (dir.exists("inputs/audit_files_refugee")) {
+    zip::zip(zipfile = "inputs/audit_files_refugee.zip", 
+             files = list.dirs(path = "inputs/audit_files_refugee/", full.names = TRUE, recursive = FALSE),
              mode = "cherry-pick")
 }
+
+
