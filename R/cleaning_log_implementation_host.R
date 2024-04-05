43722fbf422a7ca417f5d507d6e704c51a5f1993
Anthony Twesigye <twesigye10@gmail.com>
2024-04-05 11:32:23 +0300

    filtered cleaning log

--- R/cleaning_log_implementation_host.R
+++ R/cleaning_log_implementation_host.R
@@ -42,19 +42,24 @@ cols_to_remove_host <- c("audit", "audit_URL", "pt_num_msg", "pt_num_validation_
                             "_geopoint_altitude", "_geopoint_precision")
 
 
-# updating the dataset with new columns -----------------------------------
+
+# Main dataset ------------------------------------------------------------
+
+# filtered log
+df_filled_cl_host_main <- df_filled_cl_host %>% 
+    filter(is.na(sheet))
+
+# updating the main dataset with new columns
 
 df_data_with_added_cols_host <- cts_add_new_sm_choices_to_data(input_df_tool_data = df_tool_data_host,
-                                                          input_df_filled_cl = df_filled_cl_host, 
+                                                          input_df_filled_cl = df_filled_cl_host_main, 
                                                           input_df_choices = df_choices_host)
-
-# create a clean data -----------------------------------------------------
 
 # check the cleaning log
 df_cl_review_host <- cleaningtools::review_cleaning_log(
     raw_dataset = df_data_with_added_cols_host,
     raw_data_uuid_column = "_uuid",
-    cleaning_log = df_filled_cl_host,
+    cleaning_log = df_filled_cl_host_main,
     cleaning_log_change_type_column = "change_type",
     change_response_value = "change_response",
     cleaning_log_question_column = "question",
@@ -62,7 +67,7 @@ df_cl_review_host <- cleaningtools::review_cleaning_log(
     cleaning_log_new_value_column = "new_value")
 
 # filter log for cleaning
-df_final_cleaning_log_host <- df_filled_cl_host %>% 
+df_final_cleaning_log_host <- df_filled_cl_host_main %>% 
     filter(!question %in% c("duration_audit_sum_all_ms", "duration_audit_sum_all_minutes"), !uuid %in% c("all")) %>% 
 filter(!str_detect(string = question, pattern = "\\w+\\/$"))
 
@@ -80,7 +85,7 @@ df_cleaning_step_host <- cleaningtools::create_clean_data(
     cleaning_log_uuid_column = "uuid",
     cleaning_log_new_value_column = "new_value")
 
-# handle parent question columns ------------------------------------------
+# handle parent question columns
 
 df_updating_sm_parents_host <- cts_update_sm_parent_cols(input_df_cleaning_step_data = df_cleaning_step_host)
 
@@ -88,13 +93,25 @@ df_updating_sm_parents_host <- cts_update_sm_parent_cols(input_df_cleaning_step_
 list_of_datasets_host <- list("raw_data" = df_tool_data_host %>% select(-any_of(cols_to_remove_host)),
                               "cleaned_data" = df_updating_sm_parents_host$updated_sm_parents)
 
-openxlsx::write.xlsx(list_of_datasets_host, 
-                     paste0("outputs/", butteR::date_file_prefix(), "_UGA2402_aba_mbarara_host_cleaned_data.xlsx"),
-                     overwrite = TRUE)
+df_main_extra_log <- df_updating_sm_parents_host$extra_log_sm_parents
 
-# extra log for recreated select multiple ---------------------------------
+# 
+# openxlsx::write.xlsx(list_of_datasets_host, 
+#                      paste0("outputs/", butteR::date_file_prefix(), "_UGA2402_aba_mbarara_host_cleaned_data.xlsx"),
+#                      overwrite = TRUE)
+# 
+# # extra log for recreated select multiple ---------------------------------
+# 
+# openxlsx::write.xlsx(df_updating_sm_parents_host$extra_log_sm_parents, 
+#                      paste0("outputs/", butteR::date_file_prefix(), 
+#                               "_extra_sm_parent_changes_checks_aba_mbarara_host.xlsx"))
 
-openxlsx::write.xlsx(df_updating_sm_parents_host$extra_log_sm_parents, 
-                     paste0("outputs/", butteR::date_file_prefix(), 
-                              "_extra_sm_parent_changes_checks_aba_mbarara_host.xlsx"))
+
+# roster cleaning ---------------------------------------------------------
+
+
+
+
+# income_received ---------------------------------------------------------
+
 
