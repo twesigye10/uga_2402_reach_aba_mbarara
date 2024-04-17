aad5dc583873a3f949b8ac63c7607e00c493e7eb
Anthony Twesigye <twesigye10@gmail.com>
2024-04-17 08:50:11 +0300

    pushed fcs to other composites

--- R/composite_indicators.R
+++ R/composite_indicators.R
@@ -3,6 +3,19 @@ create_composite_indicators_main <- function(input_df) {
     input_df %>% 
         mutate(i.avg_family_size = rowSums(select(., any_of(c("number_hh_members_in_mbarara","number_hh_members_live_in_settlement", "number_hh_members_live_in_other_places"))), na.rm = TRUE),
                i.shelter_index = number_hh_members_in_mbarara/shelter_room_sleep_number
-        )
+        ) %>% 
+        addindicators::add_fcs(cutoffs = "normal",
+                               fsl_fcs_cereal = "fcs_cereals",
+                               fsl_fcs_legumes = "fcs_pulses",
+                               fsl_fcs_veg = "fcs_vegetables",
+                               fsl_fcs_fruit = "fcs_fruits",
+                               fsl_fcs_meat = "fcs_protein",
+                               fsl_fcs_dairy = "fcs_dairy",
+                               fsl_fcs_sugar = "fcs_sugar",
+                               fsl_fcs_oil = "fcs_oils"
+        ) %>% 
+        select(-starts_with("fcs_weight")) %>% 
+        rename(i.fcs_score = fsl_fcs_score) %>% 
+        rename(i.fcs_cat = fsl_fcs_cat) 
 }
 
--- R/preliminary_analysis_refugee.R
+++ R/preliminary_analysis_refugee.R
@@ -38,20 +38,7 @@ df_ref_pop <- read_csv("inputs/refugee_population_aba_mbarara.csv")
 # data with composites
 df_data_with_composites_refugee <- df_main_clean_data_refugee %>% 
     # create_composite_indicators() %>% 
-    mutate(strata = paste0("refugee_", interview_cell)) %>% 
-    addindicators::add_fcs(cutoffs = "normal",
-            fsl_fcs_cereal = "fcs_cereals",
-            fsl_fcs_legumes = "fcs_pulses",
-            fsl_fcs_veg = "fcs_vegetables",
-            fsl_fcs_fruit = "fcs_fruits",
-            fsl_fcs_meat = "fcs_protein",
-            fsl_fcs_dairy = "fcs_dairy",
-            fsl_fcs_sugar = "fcs_sugar",
-            fsl_fcs_oil = "fcs_oils"
-    ) %>% 
-    select(-starts_with("fcs_weight")) %>% 
-    rename(i.fcs_score = fsl_fcs_score) %>% 
-    rename(i.fcs_cat = fsl_fcs_cat) 
+    mutate(strata = paste0("refugee_", interview_cell))
     
 
 # refugee analysis --------------------------------------------------------
