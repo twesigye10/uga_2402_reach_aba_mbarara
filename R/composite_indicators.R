5b65e82ea339af8b90bcefa46cf23981617244b8
atmathias <atmathias@gmail.com>
2024-04-17 07:53:22 +0300

    adding more indicators

--- R/composite_indicators.R
+++ R/composite_indicators.R
@@ -1,5 +1,5 @@
 # creating composite indicators -------------------------------------------
-
+# main data
 create_composites <- function(input_df) {
     input_df %>% 
         mutate(i.respondent_age = case_when(respondent_age <= 20 ~ "age_18_20",
@@ -32,7 +32,31 @@ create_composites <- function(input_df) {
                                          (number_hh_members_in_mbarara + number_hh_members_live_in_settlement + number_hh_members_live_in_other_places)
                                          <= 9 ~ "between_7_and_9_members",
                                          (number_hh_members_in_mbarara + number_hh_members_live_in_settlement + number_hh_members_live_in_other_places)
-                                         >= 10 ~ "10_or_more_members")
+                                         >= 10 ~ "10_or_more_members"),
+    
+              # hh_roster 
+               i.hh_members_age_group = case_when((age >= 0 & age <= 2) ~ "age_0_2",
+                                                  (age >= 3 & age <=5) ~ "age_3_5",
+                                                  (age >=6 & age <=12) ~ "age_6_12",
+                                                  (age >=13 & age <=18) ~ "age_13_18",
+                                                  (age >=19 & age <=24) ~ "age_19_24",
+                                                  (age >=25 & age <=59) ~ "age_25_59",
+                                                  (age >= 59)  ~ "age_greater_59"),
+              i.disability_age_group = case_when((age >=5 & age <13) ~ "age_5_12",
+                                                 (age >12 & age <19) ~ "age_13_18",
+                                                 (age >18 & age <25) ~ "age_19_24",
+                                                 (age >=25 & age <60) ~ "age_25_59",
+                                                 (age > 59)  ~ "age_greater_59"),
+              i.hh_disability_prevalence =  case_when(vulnerability_see %in% c("yes_a_lot_of_difficulty","cannot_do_at_all")|vulnerability_hear %in% c("yes_a_lot_of_difficulty" , "cannot_do_at_all")|
+                                                  vulnerability_walk %in% c("yes_a_lot_of_difficulty" , "cannot_do_at_all")|vulnerability_concentrate %in% c("yes_a_lot_of_difficulty" , "cannot_do_at_all")|
+                                                  vulnerability_self_care %in% c("yes_a_lot_of_difficulty" , "cannot_do_at_all")|vulnerability_communicate %in% c("yes_a_lot_of_difficulty" , "cannot_do_at_all")~ "yes_disability",
+                                                  vulnerability_see %in% c("no_difficulty" , "yes_some_difficulty")|vulnerability_hear %in% c("no_difficulty" , "yes_some_difficulty")|
+                                                  vulnerability_walk %in% c("no_difficulty" , "yes_some_difficulty")|vulnerability_concentrate %in% c("no_difficulty" , "yes_some_difficulty")|
+                                                  vulnerability_self_care %in% c("no_difficulty" , "yes_some_difficulty")|vulnerability_communicate %in% c("no_difficulty" , "yes_some_difficulty")~
+                                                      "no_disability", TRUE ~ NA_character_),
+              i.occupation_age_group = case_when(age <= 24 ~ "age_18_24",
+                                           age <= 59 ~ "age_25_59",
+                                           age >= 60 ~ "age_greater_60"),
     
                )
 }
