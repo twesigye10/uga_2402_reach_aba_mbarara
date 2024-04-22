f1985141d141245fafeb18089e9486b36eeb42e8
Anthony Twesigye <twesigye10@gmail.com>
2024-04-22 14:14:12 +0300

    added indicators on the hh level from roster

--- R/preliminary_analysis_refugee.R
+++ R/preliminary_analysis_refugee.R
@@ -32,15 +32,97 @@ df_choices_refugee <- readxl::read_excel(loc_tool_refugee, sheet = "choices")
 # loa // list of analysis
 all_loa_refugee <- read_csv("inputs/r_loa_aba_mbarara_refugee.csv")
 
+# individual to hh level refugee ------------------------------------------
+
+list_individual_to_hh_refugee <- list()
+
+# dependency ratio
+
+df_data_dependency_refugee <- df_clean_loop_r_roster_refugee %>% 
+    mutate(int.dependency_cat = ifelse(age < 15 | age > 65, "dependant", "non_dependant"),
+           int.dependant = ifelse(int.dependency_cat %in% c("dependant"), 1, 0),
+           int.non_dependant = ifelse(int.dependency_cat %in% c("non_dependant"), 1, 0)) %>% 
+    group_by(`_submission__uuid`) %>% 
+    summarise(i.n_dependant = sum(int.dependant, na.rm = TRUE),
+              i.n_non_dependant = sum(int.non_dependant, na.rm = TRUE)) 
+add_checks_data_to_list(input_list_name = "list_individual_to_hh_refugee", input_df_name = "df_data_dependency_refugee")
+
+# member_hoh_by_gender
+df_member_hoh_by_gender_refugee <- df_clean_loop_r_roster_refugee %>% 
+    filter(member_hoh %in% c("yes")) %>% 
+    group_by(`_submission__uuid`) %>% 
+    filter(n() == 1) %>% 
+    select(`_submission__uuid`, i.member_hoh_gender = gender, i.member_hoh_age = age, i.hoh_education_level = hoh_education_level)
+add_checks_data_to_list(input_list_name = "list_individual_to_hh_refugee", input_df_name = "df_member_hoh_by_gender_refugee")
+
+# hh_with_disabled_member
+df_hh_with_disabled_member_refugee <- df_clean_loop_r_roster_refugee %>% 
+    create_composites_loop_roster_refugee() %>% 
+    group_by(`_submission__uuid`) %>% 
+    summarise(int.hh_disability_status = paste(i.disability_prevalence, collapse = " : ")) %>% 
+    mutate(i.hh_with_disabled_member = case_when(str_detect(string = int.hh_disability_status, pattern = "yes_disability") ~ "yes_disability",
+                                                 !str_detect(string = int.hh_disability_status, pattern = "yes_disability") ~ "no_disability")) %>% 
+    select(-int.hh_disability_status)
+add_checks_data_to_list(input_list_name = "list_individual_to_hh_refugee", input_df_name = "df_hh_with_disabled_member_refugee")
+
+# hoh_disability
+df_hh_with_disabled_member_refugee <- df_clean_loop_r_roster_refugee %>% 
+    filter(member_hoh %in% c("yes")) %>% 
+    create_composites_loop_roster_refugee() %>% 
+    group_by(`_submission__uuid`) %>% 
+    filter(n() == 1) %>% 
+    mutate(i.hoh_disability = case_when(i.disability_prevalence %in% c("yes_disability") ~ "yes_disability",
+                                        i.disability_prevalence %in% c("no_disability") ~ "no_disability")) %>% 
+    select(`_submission__uuid`, i.hoh_disability)
+add_checks_data_to_list(input_list_name = "list_individual_to_hh_refugee", input_df_name = "df_hh_with_disabled_member_refugee")
+
+# female_hoh_single_parent
+df_female_hoh_single_parent_refugee <- df_clean_loop_r_roster_refugee %>% 
+    filter(member_hoh %in% c("yes"), gender %in% c("female")) %>% 
+    group_by(`_submission__uuid`) %>% 
+    filter(n() == 1) %>% 
+    mutate(i.female_hoh_single_parent = female_hoh_single_parent) %>% 
+    select(`_submission__uuid`, i.female_hoh_single_parent)
+add_checks_data_to_list(input_list_name = "list_individual_to_hh_refugee", input_df_name = "df_female_hoh_single_parent_refugee")
+
+# lactating_mother
+df_hh_with_lactating_mother_refugee <- df_clean_loop_r_roster_refugee %>% 
+    group_by(`_submission__uuid`) %>% 
+    summarise(int.hh_lactating_mother = paste(lactating_mother, collapse = " : "),
+              i.num_pregnant_lactating = sum(lactating_mother %in% c("yes"), na.rm = TRUE)) %>% 
+    mutate(i.hh_with_lactating_mother = case_when(str_detect(string = int.hh_lactating_mother, pattern = "yes") ~ "yes",
+                                                  !str_detect(string = int.hh_lactating_mother, pattern = "yes") ~ "no")) %>% 
+    select(-int.hh_lactating_mother)
+add_checks_data_to_list(input_list_name = "list_individual_to_hh_refugee", input_df_name = "df_hh_with_lactating_mother_refugee")
+
+# unaccompanied_separated_or_orphan
+df_hh_with_unaccompanied_separated_or_orphan_refugee <- df_clean_loop_r_roster_refugee %>% 
+    group_by(`_submission__uuid`) %>% 
+    summarise(int.unaccompanied_separated_or_orphan = paste(unaccompanied_separated_or_orphan, collapse = " : ")) %>% 
+    mutate(i.hh_with_unaccompanied_separated_or_orphan = case_when(str_detect(string = int.unaccompanied_separated_or_orphan, pattern = "yes") ~ "yes",
+                                                                   !str_detect(string = int.unaccompanied_separated_or_orphan, pattern = "yes") ~ "no")) %>% 
+    select(-int.unaccompanied_separated_or_orphan)
+add_checks_data_to_list(input_list_name = "list_individual_to_hh_refugee", input_df_name = "df_hh_with_unaccompanied_separated_or_orphan_refugee")
+
+
+# combine the calculated indicators
+
+df_combined_hh_indicators_from_roster_refugee <- list_individual_to_hh_refugee %>%
+    reduce(.f = full_join, by = '_submission__uuid')
+
+
+# data with composites ----------------------------------------------------
+
 # main data with composites
 df_data_with_composites_refugee <- df_main_clean_data_refugee %>% 
     create_composite_indicators_main() %>%
+    left_join(df_combined_hh_indicators_from_roster_refugee, by = c("_uuid" = "_submission__uuid")) %>% 
     mutate(strata = paste0("refugee_", interview_cell)) %>% 
     filter(!interview_cell %in% c("kyamugolanyi"))
     
 # roster
-df_clean_loop_r_roster_with_composites_refugee <- df_clean_loop_r_roster_refugee #%>% 
-    # create_composite_indicators_roster()
+df_clean_loop_r_roster_with_composites_refugee <- df_clean_loop_r_roster_refugee %>% 
+    create_composite_indicators_roster()
 
 # refugee analysis - main -------------------------------------------------
 
