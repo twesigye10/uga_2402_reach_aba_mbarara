6822d771724b3db425d79628bb3cb7d686b87970
Anthony Twesigye <twesigye10@gmail.com>
2024-04-23 18:23:45 +0300

    host roster formatting

--- R/formatting_roster_analysis_host.R
+++ R/formatting_roster_analysis_host.R
@@ -1,10 +1,10 @@
 library(openxlsx)
 
 
-# refugee -----------------------------------------------------------------
+# host -----------------------------------------------------------------
 
 # run after running the analysis script
-df_analysis_wide_refugee <- df_roster_analysis_refugee$results_table %>% 
+df_analysis_wide_host <- df_roster_analysis_host$results_table %>% 
     select(-c(group_var, stat_low, stat_upp, 
               n_total, n_w, n_w_total, analysis_key)) %>% 
     mutate(group_var_value = ifelse(is.na(group_var_value), "total", group_var_value)) %>% 
@@ -12,7 +12,7 @@ df_analysis_wide_refugee <- df_roster_analysis_refugee$results_table %>%
     mutate(row_id = row_number())
     
 
-# openxlsx::write.xlsx(df_analysis_wide_refugee, "outputs/test_wide_format.xlsx")
+# openxlsx::write.xlsx(df_analysis_wide_host, "outputs/test_wide_format.xlsx")
 
 df_cols_for_ordering <- readxl::read_excel("outputs/column_ordering.xlsx", sheet = "combined")
 
@@ -22,18 +22,18 @@ reordered_columns <- df_cols_for_ordering %>%
 
 # reorder
 
-df_analysis_wide_refugee_reodered <- df_analysis_wide_refugee %>%
+df_analysis_wide_host_reodered <- df_analysis_wide_host %>%
     relocate(any_of(reordered_columns), .after = "analysis_var_value") %>% 
     relocate(analysis_type, .after = "analysis_var_value")
 
-cols_for_num_pct_formatting <- df_analysis_wide_refugee_reodered %>% 
+cols_for_num_pct_formatting <- df_analysis_wide_host_reodered %>% 
     select(stat_total:row_id) %>% 
     select(!matches("^n_"), -row_id) %>% 
     colnames()
 
 # extract header data
 
-df_to_extract_header = df_analysis_wide_refugee_reodered %>% 
+df_to_extract_header = df_analysis_wide_host_reodered %>% 
     select(-any_of(c("analysis_var", "analysis_var_value", 
               "row_id"))) %>% 
     colnames()
@@ -50,7 +50,7 @@ df_extracted_header <- bind_rows(df_extracted_header_data) %>%
 
 # create workbook ---------------------------------------------------------
 
-wb_refugee <- createWorkbook()
+wb_host <- createWorkbook()
 
 hs1 <- createStyle(fgFill = "#EE5859", halign = "CENTER", textDecoration = "Bold", fontColour = "white", fontSize = 14, wrapText = T, 
                    border = "TopBottomLeftRight", borderStyle = "medium", borderColour = "#000000")
@@ -67,22 +67,22 @@ number_style <- openxlsx::createStyle(numFmt = "0")
 # percent
 pct = createStyle(numFmt="0.0%") # not working
 
-addWorksheet(wb_refugee, sheetName="Analysis")
+addWorksheet(wb_host, sheetName="Analysis")
 
 
-# addStyle(wb_refugee, sheet = "Analysis", hs1, rows = 1, cols = 3:34, gridExpand = TRUE)
+# addStyle(wb_host, sheet = "Analysis", hs1, rows = 1, cols = 3:34, gridExpand = TRUE)
 
 # header showing results headings
-writeData(wb_refugee, sheet = "Analysis", df_extracted_header %>% head(1), startCol = 2, 
+writeData(wb_host, sheet = "Analysis", df_extracted_header %>% head(1), startCol = 2, 
           startRow = 3, headerStyle = hs2, colNames = FALSE, 
           borders = "all", borderColour = "#000000", borderStyle = "thin")
 
-setColWidths(wb = wb_refugee, sheet = "Analysis", cols = 1, widths = 70)
-setColWidths(wb = wb_refugee, sheet = "Analysis", cols = 2, widths = 10)
-setColWidths(wb = wb_refugee, sheet = "Analysis", cols = 3:90, widths = 8)
+setColWidths(wb = wb_host, sheet = "Analysis", cols = 1, widths = 70)
+setColWidths(wb = wb_host, sheet = "Analysis", cols = 2, widths = 10)
+setColWidths(wb = wb_host, sheet = "Analysis", cols = 3:90, widths = 8)
 
 # split variables to be written in different tables with in a sheet
-sheet_variables_data <- split(df_analysis_wide_refugee_reodered, factor(df_analysis_wide_refugee_reodered$analysis_var, levels = unique(df_analysis_wide_refugee_reodered$analysis_var)))
+sheet_variables_data <- split(df_analysis_wide_host_reodered, factor(df_analysis_wide_host_reodered$analysis_var, levels = unique(df_analysis_wide_host_reodered$analysis_var)))
 
 previous_row_end <- 3
 
@@ -105,13 +105,13 @@ for (i in 1:length(sheet_variables_data)) {
     print(current_row_start)
     
     # add header for variable
-    writeData(wb_refugee, sheet = "Analysis", get_question, startCol = 1, startRow = previous_row_end + 1)
-    writeData(wb_refugee, sheet = "Analysis", get_qn_type, startCol = 2, startRow = previous_row_end + 1)
-    addStyle(wb_refugee, sheet = "Analysis", hs2, rows = previous_row_end + 1, cols = 1:2, gridExpand = TRUE)
+    writeData(wb_host, sheet = "Analysis", get_question, startCol = 1, startRow = previous_row_end + 1)
+    writeData(wb_host, sheet = "Analysis", get_qn_type, startCol = 2, startRow = previous_row_end + 1)
+    addStyle(wb_host, sheet = "Analysis", hs2, rows = previous_row_end + 1, cols = 1:2, gridExpand = TRUE)
     
     current_data_length <- max(current_variable_data$row_id) - min(current_variable_data$row_id)
     
-    writeData(wb = wb_refugee, 
+    writeData(wb = wb_host, 
               sheet = "Analysis", 
               x = current_variable_data %>% 
                   select(-c(analysis_var, 
@@ -124,12 +124,12 @@ for (i in 1:length(sheet_variables_data)) {
     previous_row_end <- current_row_start + current_data_length
 }
 # hide grid lines
-# showGridLines(wb_refugee,  "Analysis", showGridLines = FALSE)  
+# showGridLines(wb_host,  "Analysis", showGridLines = FALSE)  
 
 # freeze pane
-freezePane(wb_refugee, "Analysis", firstActiveRow = 4, firstActiveCol = 3)
+freezePane(wb_host, "Analysis", firstActiveRow = 4, firstActiveCol = 3)
 
-# openXL(wb_refugee)
+openXL(wb_host)
 
-saveWorkbook(wb_refugee, paste0("outputs/", butteR::date_file_prefix(),"_formatted_analysis_aba_mbarara_roster_refugee.xlsx"), overwrite = TRUE)
-openXL(file = paste0("outputs/", butteR::date_file_prefix(),"_formatted_analysis_aba_mbarara_roster_host.xlsx"))
+# saveWorkbook(wb_host, paste0("outputs/", butteR::date_file_prefix(),"_formatted_analysis_aba_mbarara_roster_host.xlsx"), overwrite = TRUE)
+# openXL(file = paste0("outputs/", butteR::date_file_prefix(),"_formatted_analysis_aba_mbarara_roster_host.xlsx"))
